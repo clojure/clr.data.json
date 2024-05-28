41cc9b52bc02debc2d7c89fbaa86e2a28bfedd7f
David Miller <dmiller2718@gmail.com>
2024-05-28 14:08:14 -0400

    Update to Java commit 2567660 (2022.09.02): Handle end-of-file in object and array readers

--- src/main/clojure/clojure/data/json.clj
+++ src/main/clojure/clojure/data/json.clj
@@ -278,6 +278,7 @@
     (codepoint-case c
       \] []
       \, (throw (invalid-array-exception))
+	  -1 (throw (Exception. "JSON error (end-of-file inside array)"))
       (do (.Unread stream c)                                                                           ;;; .unread
           (read-array* stream options)))))
 
@@ -288,8 +289,9 @@
         (if (= (codepoint \:) (int (next-token stream)))
           key
           (throw (Exception. "JSON error (missing `:` in object)"))))
-      (if (= c (codepoint \}))
-        nil
+      (codepoint-case c
+        \} nil
+        -1 (throw (Exception. "JSON error (end-of-file inside object)"))
         (throw (Exception. (str "JSON error (non-string key in object), found `" (char c) "`, expected `\"`"))))))) 
   
 (defn- read-object [^PushbackTextReader stream options]                                              ;;; ^PushbackReader
@@ -310,6 +312,7 @@
           (codepoint-case (int (next-token stream))
             \, (recur r)
             \} (persistent! r)
+			-1 (throw (Exception. "JSON error (end-of-file inside object)"))
             (throw (Exception. "JSON error (missing entry in object)"))))
         (let [r (persistent! result)]
           (if (empty? r)
@@ -755,3 +758,4 @@
   (let [opts (merge default-write-options options)]
     (pprint/with-pprint-dispatch #(pprint-dispatch % opts)
       (pprint/pprint x))))
+
